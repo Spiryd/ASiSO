90452e0c62675c969502f8f31c182f48a98306a7
Maksymilian Neumann <neumann.maks@gmail.com>
2022-11-09 20:35:51 +0100

    a

--- L3/escseq.c
+++ L3/escseq.c
@@ -4,8 +4,10 @@ int main(int argc, char const *argv[])
 {
     for (int i = 0; i < 256; i++)
     {
-        printf("\x1b[38;5;<%i>mHello World\n", i);
+        printf("\x1B[38;5;<%i>mHello World\n", i);
     }
+
+    printf("\x1b[32mHello, World\n");
     
     return 0;
 }

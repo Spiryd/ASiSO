ab45b5cb5a4457ee0fbd4262baefbbe89a5660a7
Spiryd <neumann.maks@gmail.com>
2022-12-04 23:38:01 +0100

    test

--- L3/tolower.sh
+++ L3/tolower.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 touch  -- ' ABC DEF' '-AbC aBc' '--ABC DEF'
-find . -type f -print0 | xargs --null -Ix sh -c 'mv "$1" "$(echo "$1" | tr '[:upper:]' '[:lower:]')"' - x
+find . -type f -print0 | xargs -0 -Ix sh -c 'mv "$1" "$(echo "$1" | tr '[:upper:]' '[:lower:]')"' - x
